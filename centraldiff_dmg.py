Completed plotting in interface_flux.py, updated prosity fit in centraldiff_dmg to fix overflow error

centraldiff_dmg.py
@@ -287,7 +287,7 @@ def porcurve(z, a):
     porbottom = por[-1, 1]
     return (portop-porbottom) * np.exp(-a*z) + porbottom
 
-porfit, porcov = optimize.curve_fit(porcurve, pordepth, porvalues)
+porfit, porcov = optimize.curve_fit(porcurve, pordepth, porvalues, p0=0.001)
 
 # Solids curve fit (based on porosity curve fit function)
 def solidcurve(z, a):

interface_flux.py
@@ -10,10 +10,7 @@ Uses central difference formula of a spline fit to the upper sediment section
 
 Must run age-depth.py for site first
 
-
 How to deal with tortuosity and Dsed change w depth?
-Add in plotting of input data, processed data, and results
-
 
 """
 import numpy as np
@@ -26,18 +23,16 @@ from scipy import interpolate
 from collections import defaultdict
 from scipy import optimize, integrate
 import matplotlib.gridspec as gridspec
-
-
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 Script = os.path.basename(__file__)
 Date = datetime.datetime.now()
-
 
 # Site ID
 Leg = '315'
 Site = 'C0002'
 Holes = "('B', 'D', 'H', 'J', 'K', 'L', 'M', 'P')"
-Advection = 0  # External (hydrothermal) advection at surface
 Hole = ''.join(filter(str.isalpha, Holes))  # Formatting for saving in metadata
 
 # Species parameters
@@ -46,6 +41,10 @@ Ds = 1.875*10**-2  # m^2 per year free diffusion coefficient at 18C (ref?)
 TempD = 18  # Temperature at which diffusion coefficient is known
 Precision = 0.02  # measurement precision
 Ocean = 54.0  # Concentration in modern ocean (mM)
+
+# Model parameters
+dp = 12  # Number of concentration datapoints to use for exponential curve fit
+intervalthickness = 1  # Thickness of intervals between exponential curve fit datapoints for calcualting flux (meters)
 
 ###############################################################################
 ###############################################################################
@@ -67,7 +66,6 @@ sql = """SELECT sample_depth, {} FROM {} where leg = '{}' and site = '{}' and ho
 concdata = pd.read_sql(sql, con)
 concdata = concdata.sort_values(by='sample_depth')
 concdata = concdata.as_matrix()
-
 ct0 = [Ocean]  # mol per m^3 in modern average seawater at specific site
 
 # Porosity data
@@ -137,12 +135,22 @@ def averages(names, values):
 concunique = averages(concdata[:, 0], concdata[:, 1])
 concunique = np.concatenate((np.array(([0],ct0)).T, concunique), axis=0)
 
+'''
 # Spline fit to first 4 unique concentration values
 spline_function = interpolate.splrep(concunique[:4,0], concunique[:4,1], k=2)
 intervalthickness = 1  # meters
 conc_interp_depths = np.arange(0,3,intervalthickness)  # Zero to 2 meters
 conc_interp = interpolate.splev(conc_interp_depths, spline_function)
 conc_interp_plot = interpolate.splev(np.linspace(concunique[0,0], concunique[3,0], num=50), spline_function)
+'''
+
+# Fit exponential curve to concentration datapoints specified as "dp"
+def conc_curve(z, a):
+    return (ct0-concunique[dp-1,1]) * np.exp(-a*z) + concunique[dp-1,1]
+conc_fit, conc_cov = optimize.curve_fit(conc_curve, concunique[:dp,0], concunique[:dp,1])
+conc_interp_depths = np.arange(0,3,intervalthickness)  # Three equally-spaced points 
+conc_interp_fit = conc_curve(conc_interp_depths, conc_fit)
+conc_interp_fit_plot = conc_curve(np.linspace(concunique[0,0], concunique[dp-1,0], num=50), conc_fit)
 
 ###############################################################################
 # Porosity and solids fraction functions and data preparation
@@ -158,7 +166,7 @@ def porcurve(z, a):
     porbottom = por[-1, 1]  # Takes lowest porosity measurement as the lower boundary
     return (portop-porbottom) * np.exp(-a*z) + porbottom
 
-porfit, porcov = optimize.curve_fit(porcurve, pordepth, porvalues)
+porfit, porcov = optimize.curve_fit(porcurve, pordepth, porvalues, p0=0.001)
 
 #Sediment properties
 porosity = porcurve(conc_interp_depths, porfit)
@@ -210,48 +218,50 @@ pwburialflux = deeppor*sedmassrate/deepsolid
 ###############################################################################
 # Flux model # Needs work
 
-gradient = (-3*conc_interp[0] + 4*conc_interp[1] - conc_interp[2])/(2*intervalthickness)
-
-flux = porcurve(pordepth[0], porfit) * Dsed * gradient + (porcurve(pordepth[0], porfit) * Advection + pwburialflux) * conc_interp[0]
+gradient = (-3*conc_interp_fit[0] + 4*conc_interp_fit[1] - conc_interp_fit[2])/(2*intervalthickness)
+flux = porcurve(pordepth[0], porfit) * Dsed * gradient + (porcurve(pordepth[0], porfit) * advection + pwburialflux) * conc_interp_fit[0]
 print('Flux (mol/m^2 y^-1):', flux)
 
-figure_1, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8) = plt.subplots(1, 8, figsize = (15, 8))
-grid = gridspec.GridSpec(3, 8, wspace=0.7, hspace=0.5)
-
-ax1 = plt.subplot(grid[1:3, :2])
+###############################################################################
+# Plotting
+    
+# Set up axes and subplot grid
+figure_1, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize = (15, 7))
+grid = gridspec.GridSpec(3, 8, wspace=0.7)
+ax1 = plt.subplot(grid[0:3, :2])
 ax1.grid()
-ax2 = plt.subplot(grid[1:3, 2:4], sharey=ax1)
+ax2 = plt.subplot(grid[0:3, 2:4], sharey=ax1)
 ax2.grid()
-ax3 = plt.subplot(grid[1:3, 4:6], sharey=ax1)
+ax3 = plt.subplot(grid[0:3, 4:6], sharey=ax1)
 ax3.grid()
-ax4 = plt.subplot(grid[1:3, 6:8], sharey=ax1)
+ax4 = plt.subplot(grid[0:3, 6:8], sharey=ax1)
 ax4.grid()
-ax5 = plt.subplot(grid[0, 0:2])
-ax5.grid()
-ax6 = plt.subplot(grid[0, 2:4], sharey=ax5)
-ax6.grid()
-ax7 = plt.subplot(grid[0, 4:6], sharey=ax5)
-ax7.grid()
-ax8 = plt.subplot(grid[0, 6:8], sharey=ax5)
-ax8.grid()
 
+# Figure title
+figure_1.suptitle(r"$Expedition\ {},\ Site\ {}\ \ \ \ \ \ \ \ \ \ \ \ \ \ {}\ flux={}\ mol/m^2y$".format(Leg, Site, Solute, round(flux[0],4)), fontsize=20)
 
+# Plot input data
 ax1.plot(concunique[:,1], concunique[:,0], 'go', label='Measured')
-ax1.plot(concunique[0:4,1], concunique[0:4,0], 'bo', label="Fit points")
+ax1.plot(concunique[0:dp,1], concunique[0:dp,0], 'bo', label="Fit points")
 ax2.plot(por[:, 1], por[:, 0], 'mo', label='Measured')
 ax2.plot(porcurve(pordepth, porfit), pordepth, 'k-', label='Curve fit', linewidth=3)
 ax3.plot(sedtemp(np.arange(concunique[-1,0]), bottom_temp), np.arange(concunique[-1,0]), 'k-', linewidth=3)
 ax4.plot(picks[:,1]/1000000, picks[:,0], 'ro', label='Picks')
 ax4.plot(sedtimes/1000000, seddepths, 'k-', label='Curve fit', linewidth=2)
-ax5.plot(concunique[:7,1], concunique[:7,0], 'go', label='Measured')
-ax5.plot(concunique[:4,1], concunique[:4,0], 'bo', label='Measured')
-ax5.plot(conc_interp_plot, np.linspace(concunique[0,0], concunique[3,0], num=50), 'k-', label='Interpolated')
-ax6.plot(por[:len(por[:, 0].clip([0,concunique[6,0]])), 1], por[:, 0].clip([0,concunique[6,0]]), 'mo', label='Measured')
-ax6.plot(porcurve(pordepth, porfit), pordepth.clip([0,concunique[6,0]]), 'k-', label='Curve fit', linewidth=3)
-ax7.plot(sedtemp(np.arange(concunique[-1,0]), bottom_temp), np.arange(concunique[6,0]), 'k-', linewidth=3)
-ax8.plot(picks[:,1]/1000000, picks[:,0].clip([0,concunique[6,0]]), 'ro', label='Picks')
-ax8.plot(sedtimes/1000000, seddepths.clip([0,concunique[6,0]]), 'k-', label='Curve fit', linewidth=2)
 
+# Inset in concentration plot
+y2= np.ceil(concunique[dp+2,0])
+x2= max(concunique[:dp+3,1])
+x1= np.ceil(concunique[dp+2,1])
+axins1 = inset_axes(ax1, width="50%", height="30%", loc=5) # zoom = 2x
+axins1.plot(concunique[:,1], concunique[:,0], 'go')
+axins1.plot(concunique[0:dp,1], concunique[0:dp,0], 'bo')
+axins1.plot(conc_interp_fit_plot, np.linspace(concunique[0,0], concunique[dp-1,0], num=50), 'k-')
+axins1.set_xlim(x1-1, x2+1) # apply the x-limits
+axins1.set_ylim(0, y2) # apply the y-limits
+mark_inset(ax1, axins1, loc1=1, loc2=2, fc="none", ec="0.5")
+
+# Additional formatting
 ax1.legend(loc='best', fontsize='small')
 ax2.legend(loc='best', fontsize='small')
 ax4.legend(loc='best', fontsize='small')
@@ -260,21 +270,11 @@ ax1.set_xlabel('Concentration (mM)')
 ax2.set_xlabel('Porosity')
 ax3.set_xlabel('Temperature (\u00b0C)')
 ax4.set_xlabel('Age (Ma)')
-ax5.set_ylabel('Depth (mbsf)')
 ax1.locator_params(axis='x', nbins=4)
 ax2.locator_params(axis='x', nbins=4)
 ax3.locator_params(axis='x', nbins=4)
 ax4.locator_params(axis='x', nbins=4)
-ax5.locator_params(axis='x', nbins=4)
-ax6.locator_params(axis='x', nbins=4)
-ax7.locator_params(axis='x', nbins=4)
-ax8.locator_params(axis='x', nbins=4)
 ax1.invert_yaxis()
-ax5.set_ylim([0,concunique[6,0]])
-ax5.invert_yaxis()
-
-
-
-
+axins1.invert_yaxis()
 
 # eof